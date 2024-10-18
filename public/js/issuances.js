7fa2c5cfb9e766562f7a3952a3c5cd1628077448
kostasmov <kostasmov@mail.ru>
2024-10-18 22:00:52 +0300

    Issuance buttons display

--- public/js/issuances.js
+++ public/js/issuances.js
@@ -19,9 +19,34 @@ function selectRow(event) {
     const statusSpan = cells[4].querySelector('.status');
     const issuanceStatus = statusSpan.classList[1];
 
-    console.log(issuanceStatus);
+    const id = selected.getAttribute('data-id');
+
+    updateButtons(issuanceStatus, id);
+}
+
+function updateButtons(status, id) {
+    acceptButton.style.display = 'none';
+    rejectButton.style.display = 'none';
+    returnButton.style.display = 'none';
+
+    // acceptButton.removeEventListener('click', submitCreate);
+    // rejectButton.removeEventListener('click', submitEdit);
+    // returnButton.removeEventListener('click', submitCreate);
+
+    switch (status) {
+        case 'pending':
+            acceptButton.style.display = 'inline-block';
+            rejectButton.style.display = 'inline-block';
+            break;
+        case 'issued':
+            returnButton.style.display = 'inline-block';
+            break;
+        default:
+            break;
+    }
 }
 
 for (let i = 1; i < rows.length; i++) {
     rows[i].addEventListener('click', selectRow);
 }
+
